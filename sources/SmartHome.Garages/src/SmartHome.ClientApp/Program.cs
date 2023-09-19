d250069824770403debc64cd8d498feb2e2e43a8
Robert Jureczko <robert.jureczko@proton.me>
2023-09-19 21:54:08 +0200

    feat(client-app): created clear vue3 project, made first modules, added generated client api, deleted old client

--- sources/SmartHome.Garages/src/SmartHome.ClientApp/Program.cs
+++ sources/SmartHome.Garages/src/SmartHome.ClientApp/Program.cs
@@ -13,7 +13,7 @@ builder.Services
       options.SetCustomHostName(new Uri(customHostName));
     }
 
-    options.SessionCookieName = "datacarweb.session";
+    options.SessionCookieName = "smarthome.session";
 
     var idpOptions = builder.Configuration.GetSection("OpenIDConnect").Get<OpenIdConnectConfig>()!;
     options.RegisterIdentityProvider<IdentityProvider, OpenIdConnectConfig>(idpOptions);
@@ -34,6 +34,7 @@ app.UseHttpsRedirection();
 // app.UseHttpLogging();
 
 app.UseRouting();
+app.UseSecurityBff();
 
 // serve static files as a fallback, so if route has not matched any configured reverse proxy path
 // then emit static files (SPA app) and fallback to index.html
