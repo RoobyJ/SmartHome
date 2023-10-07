cd591f2bd026a9ee6952a1d25cc7781fee914c4c
Robert Jureczko <robert.jureczko@proton.me>
2023-10-07 19:26:58 +0200

    ,

--- sources/SmartHome.Garages/src/SmartHome.ClientApp/ClientApp/src/modules/core/services/api-clients/garages-client.ts
+++ sources/SmartHome.Garages/src/SmartHome.ClientApp/ClientApp/src/modules/core/services/api-clients/garages-client.ts
@@ -8,8 +8,21 @@ import { httpClient } from '../api/http-client';
 export class GaragesClient {
     private static readonly urlBase = '/api';
 
-    /** Gets application for the current organization */
+    /** Gets all garages */
     public static async getGarages(): Promise<ApiResponse<GarageDetailsDto[]>> {
+        const url = GaragesClient.urlBase + '/garages';
+        const request = httpClient.get(url).json<GarageDetailsDto[]>();
+        const apiResponse = await createApiResponse(request);
+
+        if (apiResponse.isSuccess) return apiResponse;
+
+        const { processError } = useErrorStore();
+        await processError(apiResponse.error);
+        return apiResponse;
+    }
+
+     /** Gets all heat request for garage */
+     public static async getHeatRequests(): Promise<ApiResponse<GarageDetailsDto[]>> {
         const url = GaragesClient.urlBase + '/garages';
         const request = httpClient.get(url).json<GarageDetailsDto[]>();
         const apiResponse = await createApiResponse(request);
